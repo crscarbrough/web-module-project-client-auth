f2d2d7c8d0d5719cd6132f44be21f43bc77d72e9
Claudia Scarbrough <rubyharrts@gmail.com>
2022-01-25 16:09:20 -0600

    working on logout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Link,
+} from "react-router-dom";
+
+import Login from "./components/Login";
+import FriendsList from "./components/FriendsList";
+import AddFriend from "./components/AddFriend";
+import Logout from "./components/Logout";
 
 function App() {
   return (
-    <div className="App">
-      <h2>Client Auth Project</h2>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <h2>Friends Database</h2>
+          <Link className="link" to="login">
+            Login
+          </Link>
+          <Link className="link" to="friends">
+            Friends List
+          </Link>
+          <Link className="link" to="friends/add">
+            Add Friends
+          </Link>
+          <Link className="link" to="logout">
+            Logout
+          </Link>
+        </header>
+
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route exact path="/login">
+          <Redirect to="/" />
+        </Route>
+
+        <Route exact path="/friends">
+          <FriendsList />
+        </Route>
+        <Route exact path="/friends/add">
+          <AddFriend />
+        </Route>
+        <Route exact path="/logout">
+          <Logout />
+        </Route>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Logout.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+
+const Logout = () => {
+  const { push } = useHistory();
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    axios
+      .post(
+        "http://localhost:9000/api/logout",
+        {},
+        {
+          headers: {
+            authorization: token,
+          },
+        }
+      )
+      .then((resp) => {
+        localStorage.removeItem("token");
+        push("/login");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return <div></div>;
+};
+
+export default Logout;
